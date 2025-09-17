dd7c245070d3f7308cf7b4e3972f6ec854373539
Caetano-Ventura-Coutinho <currytano@gmail.com>
2025-09-17 01:03:44 -0300

    Inserindo produtos

--- script.sql
+++ script.sql
@@ -29,4 +29,11 @@ Quantidade int,
 PrecoUnitario decimal(10,2)
 );
 
-Select id, nome, descricao, preco, imageUrl, Estoque from produtos
+Select id, nome, descricao, preco, imageUrl, Estoque from produtos;
+
+insert into produtos (nome, descricao, preco, imageUrl, Estoque) values ("Beyblade BigBang Pegasus", "Beyblade de alta qualidade, original", 37.99, "beyblade.jpg", 50);
+insert into produtos (nome, descricao, preco, imageUrl, Estoque) values ("Pelúcia Kirby", "Pelúcia do personagem Kirby", 15.99, "kirby.jpg", 100);
+insert into produtos (nome, descricao, preco, imageUrl, Estoque) values ("Boneco do Artrópode do Ben 10", "Brinquedo do Alien Artrópode da série animada Ben 10", 25.99, "artropode.jpg", 50);
+insert into produtos (nome, descricao, preco, imageUrl, Estoque) values ("Amiibo Kazuya Mishima", "Amiibo do Kazuya Mishima da sérei de jogos Tekken", 249.99, "kazuya.jpg", 30);
+insert into produtos (nome, descricao, preco, imageUrl, Estoque) values ("Omanjuu Tetora Nagumo", "Omanju do personagem Tetora Nagumo de Ensemble Stars!!", 49.99, "Tetora.jpg", 50);
+insert into produtos (nome, descricao, preco, imageUrl, Estoque) values ("Action Figure Knight (Hollow Knight)", "Action figure de alta qualidade do personagem Knight de Hollow Knight", 499.99, "Knight.jpg", 25);
