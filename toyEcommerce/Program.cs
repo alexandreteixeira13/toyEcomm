468ef2bbff10caa7c730238f7b674421bb151cf4
Caetano-Ventura-Coutinho <currytano@gmail.com>
2025-09-16 23:05:27 -0300

    Configurando a sessão

--- toyEcommerce/Program.cs
+++ toyEcommerce/Program.cs
@@ -1,7 +1,21 @@
+using toyEcommerce.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+
+});
+builder.Services.AddSingleton<string>(
+    builder.Configuration.GetConnectionString("DefaultConnection")!);
+
+builder.Services.AddScoped<ProdutoRepositorio>();
+builder.Services.AddScoped<CarrinhoRepositorio>();
 
 var app = builder.Build();
 
@@ -14,6 +28,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseSession();
 
 app.UseAuthorization();
 
